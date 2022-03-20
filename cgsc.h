c611d4e2b5b0a1d1da4d7b7b46de99496885e660
Iswenzz <suxlolz1528@gmail.com>
2022-03-20 17:19:43 +0100

    Definitions: CGSC & Plugins
    Header to be used to compile CoD4x or a plugin using CGSC (W.I.P)

--- cgsc.h
+++ cgsc.h
@@ -1,65 +1,124 @@
 #pragma once
-#include "../scr_vm.h"
 
-#ifndef PLUGIN_HANDLER_VERSION_MAJOR
-#include "../../plugins/plugin_declarations.h"
-#endif
+#ifdef COD4X18UPDATE
+	// Definitions for compiling CGSC with CoD4x
+	#include "../scr_vm.h"
+	#ifndef PLUGIN_HANDLER_VERSION_MAJOR
+		#include "../../plugins/plugin_declarations.h"
+	#endif
 
-#ifdef PLUGIN_HANDLER_VERSION_MAJOR
-    #if PLUGIN_HANDLER_VERSION_MAJOR >= 4
-		#define _CGSC_4
-        #include "../cscr_variable.h"
-        #include "../cscr_stringlist.h"
+	#ifdef PLUGIN_HANDLER_VERSION_MAJOR
+		#if PLUGIN_HANDLER_VERSION_MAJOR >= 4
+			#define _CGSC_4
+			#include "../cscr_variable.h"
+			#include "../cscr_stringlist.h"
 
-        extern struct scrVarGlob_t gScrVarGlob;
-        extern unsigned int Scr_AllocString(const char *s);
-        extern void Scr_AddIString(const char *value);
-        extern void IncInParam();
-		
-        #define IGScrVmPub gScrVmPub
-		#define IGScrVmGlob gScrVmGlob
-        #define IGScrVarGlob gScrVarGlob.variableList
+			extern struct scrVarGlob_t gScrVarGlob;
+			extern unsigned int Scr_AllocString(const char *s);
+			extern void Scr_AddIString(const char *value);
+			extern void IncInParam();
 
-    #elif PLUGIN_HANDLER_VERSION_MAJOR >= 3
-		#define _CGSC_3
-        #define IGScrVmPub scrVmPub
-        #define IGScrVarGlob scrVarGlob
+			#define IGScrVmPub gScrVmPub
+			#define IGScrVmGlob gScrVmGlob
+			#define IGScrVarGlob gScrVarGlob.variableList
 
-		extern char* SL_ConvertToString(unsigned int index);
-        extern char* var_typename[];
+		#elif PLUGIN_HANDLER_VERSION_MAJOR >= 3
+			#define _CGSC_3
+			#define IGScrVmPub scrVmPub
+			#define IGScrVarGlob scrVarGlob
 
-		enum $0E0E04F36A22A28F2C0A7A22DC12DAE9
+			extern char* SL_ConvertToString(unsigned int index);
+			extern char* var_typename[];
+
+			enum $0E0E04F36A22A28F2C0A7A22DC12DAE9
+			{
+				VAR_UNDEFINED = 0x0,
+				VAR_BEGIN_REF = 0x1,
+				VAR_POINTER = 0x1,
+				VAR_STRING = 0x2,
+				VAR_ISTRING = 0x3,
+				VAR_VECTOR = 0x4,
+				VAR_END_REF = 0x5,
+				VAR_FLOAT = 0x5,
+				VAR_INTEGER = 0x6,
+				VAR_CODEPOS = 0x7,
+				VAR_PRECODEPOS = 0x8,
+				VAR_FUNCTION = 0x9,
+				VAR_STACK = 0xA,
+				VAR_ANIMATION = 0xB,
+				VAR_DEVELOPER_CODEPOS = 0xC,
+				VAR_INCLUDE_CODEPOS = 0xD,
+				VAR_THREAD = 0xE,
+				VAR_NOTIFY_THREAD = 0xF,
+				VAR_TIME_THREAD = 0x10,
+				VAR_CHILD_THREAD = 0x11,
+				VAR_OBJECT = 0x12,
+				VAR_DEAD_ENTITY = 0x13,
+				VAR_ENTITY = 0x14,
+				VAR_ARRAY = 0x15,
+				VAR_DEAD_THREAD = 0x16,
+				VAR_COUNT = 0x17,
+				VAR_THREAD_LIST = 0x18,
+				VAR_ENDON_LIST = 0x19
+			};
+		#endif
+
+		struct __attribute__((aligned(64))) scrVarGlob_t
 		{
-			VAR_UNDEFINED = 0x0,
-			VAR_BEGIN_REF = 0x1,
-			VAR_POINTER = 0x1,
-			VAR_STRING = 0x2,
-			VAR_ISTRING = 0x3,
-			VAR_VECTOR = 0x4,
-			VAR_END_REF = 0x5,
-			VAR_FLOAT = 0x5,
-			VAR_INTEGER = 0x6,
-			VAR_CODEPOS = 0x7,
-			VAR_PRECODEPOS = 0x8,
-			VAR_FUNCTION = 0x9,
-			VAR_STACK = 0xA,
-			VAR_ANIMATION = 0xB,
-			VAR_DEVELOPER_CODEPOS = 0xC,
-			VAR_INCLUDE_CODEPOS = 0xD,
-			VAR_THREAD = 0xE,
-			VAR_NOTIFY_THREAD = 0xF,
-			VAR_TIME_THREAD = 0x10,
-			VAR_CHILD_THREAD = 0x11,
-			VAR_OBJECT = 0x12,
-			VAR_DEAD_ENTITY = 0x13,
-			VAR_ENTITY = 0x14,
-			VAR_ARRAY = 0x15,
-			VAR_DEAD_THREAD = 0x16,
-			VAR_COUNT = 0x17,
-			VAR_THREAD_LIST = 0x18,
-			VAR_ENDON_LIST = 0x19
+			VariableValueInternal *variableList;
 		};
-    #endif
+	#endif
+#else
+	// Definitions for compiling a plugin with CGSC
+	#ifndef PLUGIN_INCLUDES
+		#include "../../../pinc.h"
+	#endif
+
+	#ifdef PLUGIN_HANDLER_VERSION_MAJOR
+		#if PLUGIN_HANDLER_VERSION_MAJOR >= 4
+			#define _CGSC_4
+		#elif PLUGIN_HANDLER_VERSION_MAJOR >= 3
+			#define _CGSC_3
+		#endif
+	#endif
+
+	#ifndef SCR_ENTREF_DEFINED
+		#define SCR_ENTREF_DEFINED
+	#endif
+
+	typedef struct
+	{
+		uint16_t entnum;
+		uint16_t classnum;
+	} Plugin_Scr_entref_t;
+
+	struct VariableStackBuffer
+	{
+		const char *pos;
+		uint16_t size;
+		uint16_t bufLen;
+		uint16_t localId;
+		char time;
+		char buf[1];
+	};
+
+	union VariableUnion
+	{
+		int intValue;
+		float floatValue;
+		unsigned int stringValue;
+		const float *vectorValue;
+		const char *codePosValue;
+		unsigned int pointerValue;
+		struct VariableStackBuffer *stackValue;
+		unsigned int entityOffset;
+	};
+
+	typedef struct
+	{
+		union VariableUnion u;
+		int type;
+	} VariableValue;
 #endif
 
 #define VAR_STAT_MASK 0x60
@@ -85,6 +144,7 @@
 #define SCR_GET_ENTITY_FROM_ENTCLIENT(entcl) (entcl & UNK_ENTNUM_MASK)
 #define MAX_LOCAL_CENTITIES 1536
 #define OBJECT_STACK 0x18001u
+#define DEBUG_REFCOUNT_SIZE 65536
 
 enum GSCTypeFlag
 {
@@ -118,21 +178,15 @@ enum GSCTypeFlag
 	FLAG_ENDON_LIST = 134217728
 };
 
-typedef struct
-{
-    uint32_t length;
-    VariableValue **items;
-} VariableValueArray;
-
-struct __attribute__((aligned(64))) scrVarGlob_t
-{
-    VariableValueInternal *variableList;
-};
-
-#define DEBUG_REFCOUNT_SIZE 65536
 struct scrStringDebugGlob_t
 {
     volatile int refCount[DEBUG_REFCOUNT_SIZE];
     volatile int totalRefCount;
     int ignoreLeaks;
 };
+
+typedef struct
+{
+	uint32_t length;
+	VariableValue **items;
+} VariableValueArray;
