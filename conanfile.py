a3ef5669dd30e6c419430891d376534acbc5bc2c
Iswenzz <suxlolz1528@gmail.com>
2022-03-25 21:09:12 +0100

    Version: 1.1.0

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class CGSC(ConanFile):
 	name = "CGSC"
-	version = "1.0.0"
+	version = "1.1.0"
 	license = "LICENCE"
 	url = "https://github.com/Iswenzz/CGSC"
 	description = "Call of Duty 4X source extension that adds new features for creating plugins not offered in the original source."
