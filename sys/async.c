c72e3da5a54ab42ca45adb2d461ecdf675fd60ec
Iswenzz <suxlolz1528@gmail.com>
2022-12-08 23:22:00 +0100

    Async: libuv asynchronous calls with thread pool

--- sys/async.c
+++ sys/async.c
@@ -1,7 +1,26 @@
 #include "async.h"
-#include <uv.h>
 
-void Async_HandlerInitialize()
+void AsyncShutdown()
 {
-	uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+	uv_loop_close(uv_default_loop());
+}
+
+void AsyncCall(void *data, uv_work_cb callback, uv_after_work_cb callbackDone)
+{
+	uv_work_t *req = (uv_work_t *)malloc(sizeof(uv_work_t));
+	req->data = data;
+
+	if (!callbackDone)
+		callbackDone = &AsyncNull;
+	uv_queue_work(uv_default_loop(), req, callback, callbackDone);
+}
+
+void AsyncNull(uv_work_t* req, int status)
+{
+	AsyncFree(req);
+}
+
+void AsyncFree(uv_work_t* req)
+{
+	free(req);
 }
--- sys/async.h
+++ sys/async.h
@@ -1,10 +1,20 @@
 #pragma once
+#include <uv.h>
 
-/// @brief Initialize the async workers.
-void Async_HandlerInitialize();
+/// @brief Shutdown all async operations.
+void AsyncShutdown();
 
-/// @brief Shutdown the async workers.
-void Async_HandlerShutdown();
+/// @brief Make an async call.
+/// @param data - The data to pass.
+/// @param callback - The async callback.
+/// @param callbackDone - The callback when the async operation is completed.
+void AsyncCall(void *data, uv_work_cb callback, uv_after_work_cb callbackDone);
 
-/// @brief Call a function asynchronously.
-void Async_Call();
+/// @brief Default after worker callback.
+/// @param req - The worker request.
+/// @param status - The request status.
+void AsyncNull(uv_work_t* req, int status);
+
+/// @brief Free a worker request.
+/// @param req - The worker request.
+void AsyncFree(uv_work_t* req);
