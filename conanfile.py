2ebfe0740be52cb277c6851b900c191a29fe9c5a
Iswenzz <suxlolz1528@gmail.com>
2022-12-10 15:55:25 +0100

    CMake: Package plugin/server usage

--- conanfile.py
+++ conanfile.py
@@ -13,33 +13,27 @@ class CGSC(ConanFile):
     requires = ["libuv/1.44.2"]
 
     settings = "os", "arch", "compiler", "build_type"
-    options = {"enable_testing": [True, False]}
-    default_options = {"enable_testing": False}
+    options = {"enable_testing": [True, False], "plugin": [True, False]}
+    default_options = {"enable_testing": False, "plugin": True}
     no_copy_source = True
 
     def build_requirements(self):
         if self.options.enable_testing:
             self.build_requires("greatest/1.5.0", force_host_context=True)
 
-    def export_sources(self):
-        self.copy("cgsc.h", dst="CGSC")
-        self.copy("plugin.h", dst="CGSC")
-        self.copy("*.h", src="extensions", dst="CGSC/extensions")
-        self.copy("*.h", src="sys", dst="CGSC/sys")
-        self.copy("*.h", src="utils", dst="CGSC/utils")
-        self.copy("*.h", src="versions", dst="CGSC/versions")
-
     def build(self):
         cmake_release = CMake(self, build_type="Release")
         cmake_release.definitions["CONAN_DISABLE_CHECK_COMPILER"] = True
-        cmake_release.definitions["PLUGIN"] = True
+        if self.options.plugin:
+            cmake_release.definitions["PLUGIN"] = True
+        else:
+            cmake_release.definitions["COD4X18UPDATE"] = True
         cmake_release.configure()
         cmake_release.build()
 
+    def export_sources(self):
+        self.copy("*.h", dst="CGSC", keep_path=True)
+
     def package(self):
         self.copy("LICENSE, README.md")
-        self.copy("*.h", dst="include", excludes="cod4x")
-        self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="bin", keep_path=False)
+        self.copy("*.h", dst="include")
